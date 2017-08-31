6ef713fb196b1d456dd87b0a789a8ac2e68a1835
Dannyfk <dfk2306@hotmail.com>
2017-08-31 13:47:12 +0200

    Updated the index and associated framework and js scripts

--- js/frameWorkScript.js
+++ js/frameWorkScript.js
@@ -17,3 +17,31 @@ const app2 = new Vue({
         items: [] = friendList
     }
 });
+
+// Looping the friendlist and adding color and outlocation if the person has one
+var ul = document.getElementById("friendListObject");
+var items = ul.getElementsByTagName("li");
+for (var i = 0; i < items.length; ++i) {
+    var temp = ul.childNodes[i].innerHTML;
+    
+    var tempPerson ="";
+    var tempNumber = 0;
+    var found = false;
+    while(found != true && tempNumber<friendList.length){
+        if (friendList[tempNumber].firstName === temp.trim()){
+            tempPerson = friendList[tempNumber];
+            tempNumber=0;
+            found=true;
+        }
+        else {
+            tempNumber ++;
+        }
+    }
+    if(tempPerson.out === true){
+        ul.childNodes[i].setAttribute("class","true");
+        ul.childNodes[i].innerHTML = ul.childNodes[i].innerHTML + "<br>-" + tempPerson.outPlace;
+    }
+    else {
+        ul.childNodes[i].setAttribute("class","false");
+    }
+}
--- updateProfile.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST['goingOut']) && !empty($_POST['location'])) {
-	//do something with the update
-} else if(isset($_POST['goingOut']) && empty($_POST['location'])){
-	echo("please fill the location to update");
-}
-header('Location: index.php');
-?>
